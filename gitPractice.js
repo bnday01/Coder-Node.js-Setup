fef3042c81c4a042977d5f055eec6e8f1fb7ce5e
Bryan Day <bnday01@msn.com>
2018-08-07 04:41:08 +0000

    final practice commit

--- gitPractice.js
+++ gitPractice.js
@@ -6,4 +6,19 @@ console.log("Hello World");
 function getValue(arr) {
     return arr.indexOf("7");
 }
-so you tellin me that the changes didnt register ?   
+//so you tellin me that the changes didnt register ?   
+//ok so as soon as you merge them together the changes automaticaly register
+
+//bueno lets write another function
+
+let job = true;
+function getMoney(job) {
+    if (job != true) {
+        return false;
+    }
+    else {
+        console.log("git money");
+    }
+}
+getMoney(job);
+//yeah yeah
