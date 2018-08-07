a772de81a08cbd58fd57dd5b3401b45adb9f9e71
Bryan Day <bnday01@msn.com>
2018-08-07 04:25:07 +0000

    first branch, this is pretty cool

--- gitPractice.js
+++ gitPractice.js
@@ -1,2 +1,9 @@
 console.log("Hello World");
 //How do I manage to preview my code on Coder? I DONT KNOW
+//Now im gonna make some more changes
+//how about a function? idk lets see what we have to do
+
+function getValue(arr) {
+    return arr.indexOf("7");
+}
+so you tellin me that the changes didnt register ?   
